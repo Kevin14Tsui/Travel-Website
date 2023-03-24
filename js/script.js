0b2246b35e92b2dc454f4ab4fe10f0195d0cef0d
Kevin Tsui <kevintsui@KevindeMacBook-Air.local>
2023-03-25 03:14:22 +0800

    bg

--- js/script.js
+++ js/script.js
@@ -7,3 +7,9 @@ document.querySelector("#menu-btn").onclick = () => {
 window.onscroll = () => {
   navbar.classList.remove("active");
 };
+
+document.querySelector('input[type="number"]').forEach(inputNumber => {
+  inputNumber.oninput = () => {
+    if (inputNumber.value.length > inputNumber.maxLength) inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
+  };
+});
